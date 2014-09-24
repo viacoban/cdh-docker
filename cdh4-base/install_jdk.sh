4b477333b10e8d59bf66dcf256dd0a0ac2046ef4
Victor Iacoban <viacoban@adobe.com>
2014-09-24 15:43:38 -0700

    start for cdh4-dev container

--- cdh4-base/install_jdk.sh
+++ cdh4-base/install_jdk.sh
@@ -5,8 +5,7 @@ mkdir -p /usr/java
 pushd /usr/java
 
 if [ ! -f jdk-7u67-linux-x64.tar.gz ]; then
-  wget --no-cookies \
-    --no-check-certificate \
+  wget --no-verbose --no-cookies --no-check-certificate \
     --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
     http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.tar.gz
 fi
@@ -16,11 +15,5 @@ if [ ! -d jdk1.7.0_67 ]; then
 fi
 
 ln -sfn /usr/java/jdk.1.7.0_67 /usr/java/default
-
-mkdir -p /etc/default
-#echo "export JAVA_HOME=/usr/local/default" >> /etc/default/bigtop-utils
-
-echo "export JAVA_HOME=/usr/local/default" >> ~/.bashrc
-echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.bashrc
 
 popd
